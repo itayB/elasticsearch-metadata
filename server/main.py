2386fa8f056d8ee4719790c574cb989cce813e8a
Itay Bittan <itay.bittan@gmail.com>
2019-12-13 22:37:25 +0200

    initial work

---
+++ server/main.py
@@ -0,0 +1,28 @@
+from aiohttp import web
+from aiohttp.web_app import Application
+from handlers.get_indices_handler import get_indices_handler
+
+STATIC_WEB_PATH = '../build'
+
+
+async def get_index(request):
+    return web.FileResponse(STATIC_WEB_PATH + '/index.html')
+
+
+def main():
+    """
+    main function
+    """
+    app: Application = web.Application()
+    app['es_conn'] = 'http://localhost:9200'
+    app.add_routes([
+        # web.get('/', get_status_handler),
+        web.get('/api/v1/indices', get_indices_handler),
+        web.get('/', get_index),  # redirect to homepage
+        web.static('/', STATIC_WEB_PATH)
+    ])
+    web.run_app(app, port=8080)
+
+
+if __name__ == '__main__':
+    main()
