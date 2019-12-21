6719474a989dd3526cd7049865357d7e53727992
Itay Bittan <itay.bittan@gmail.com>
2019-12-21 13:42:35 +0200

    remove ConfirmationDialog

--- src/Components/Indices/Dialogs/ColumnsDialog.js
+++ src/Components/Indices/Dialogs/ColumnsDialog.js
@@ -1,7 +1,6 @@
 
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -103,67 +102,3 @@ ColumnsDialog.propTypes = {
     // value: PropTypes.string.isRequired,
     columns: PropTypes.array.isRequired,
 };
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-    },
-    paper: {
-        width: '80%',
-        maxHeight: 435,
-    },
-}));
-
-// export default function ConfirmationDialog() {
-//     const classes = useStyles();
-//     const [open, setOpen] = React.useState(false);
-//     const [value, setValue] = React.useState('Dione');
-//
-//     const handleClickListItem = () => {
-//         setOpen(true);
-//     };
-//
-//     const handleClose = newValue => {
-//         setOpen(false);
-//
-//         if (newValue) {
-//             setValue(newValue);
-//         }
-//     };
-//
-//     return (
-//         <div className={classes.root}>
-//             <List component="div" role="list">
-//                 <ListItem button divider disabled role="listitem">
-//                     <ListItemText primary="Interruptions" />
-//                 </ListItem>
-//                 <ListItem
-//                     button
-//                     divider
-//                     aria-haspopup="true"
-//                     aria-controls="ringtone-menu"
-//                     aria-label="phone ringtone"
-//                     onClick={handleClickListItem}
-//                     role="listitem"
-//                 >
-//                     <ListItemText primary="Phone ringtone" secondary={value} />
-//                 </ListItem>
-//                 <ListItem button divider disabled role="listitem">
-//                     <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-//                 </ListItem>
-//                 <ColumnsDialog
-//                     classes={{
-//                         paper: classes.paper,
-//                     }}
-//                     id="ringtone-menu"
-//                     keepMounted
-//                     open={open}
-//                     onClose={handleClose}
-//                     value={value}
-//                 />
-//             </List>
-//         </div>
-//     );
-// }
