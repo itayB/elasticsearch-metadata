f8260ed841b84bd00cd73bea23994ec78e36a302
Itay Bittan <itay.bittan@gmail.com>
2019-12-25 08:52:28 +0200

    add link to github

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
-
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default props =>
     <AppBar position="static">
         <Toolbar>
             <IconButton edge="start" color="inherit" aria-label="menu">
             </IconButton>
-            <Typography variant="h6">
+            <Typography variant="h6" style={{flexGrow: 1}}>
                 Elasticsearch Metadata
             </Typography>
+            <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                href="https://github.com/itayB/elasticsearch-metadata"
+                target="_blank"
+            >
+                <GitHubIcon />
+            </IconButton>
         </Toolbar>
     </AppBar>
